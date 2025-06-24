8898637f801846fd336fd3a3cd4cafe1148ae981
Jinfeng, Pang <jfpang@ea.com>
2025-06-24 10:42:12 +0800

    fix: unnessary files

--- src/pages/Cost.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Cost() {
-  return <div style={{ fontSize: 22, fontWeight: 700 }}>这里是结费管理页面</div>;
-}
- 
-export default Cost; 
--- src/pages/Customer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Customer() {
-  return <div style={{ fontSize: 22, fontWeight: 700 }}>这里是客户管理页面</div>;
-}
- 
-export default Customer; 
